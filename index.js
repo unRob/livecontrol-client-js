0cb8524561e4a9833aeb2749165727a968e707ef
rob <rob@partidosurrealista.com>
2015-07-19 16:05:29 -0500

    GREAT SUCCESS!

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
-// var mdns = require('mdns');
+var mdns = require('mdns');
 var ws = require('nodejs-websocket');
 var SerialPort = require("serialport").SerialPort;
 
-// var browser = mdns.createBrowser(mdns.tcp('_livecontrol'));
+var browser = mdns.createBrowser(mdns.tcp('livecontrol'));
 
-// // browser.on('serviceUp', function(service) {
-// //   console.log("service up: ", service);
-// // });
-// browser.start();
+browser.on('serviceUp', function(service) {
+  console.log("service up: ", service.host);
+  var server = service.host;
+  var port = service.port;
+  found_server(server, port);
+});
+browser.start();
 
-var server = "turing.local";
-var port = 49170;
+var client = null;
 
 var found_server = function(server, port){
-	ws.connect("ws://"+server+":"+port+"/control", function(){
+	client = ws.connect("ws://"+server+":"+port+"/control", function(){
 		console.log("WS connected");
 	});
 
 	setup_tty();
 };
 
+var is_recording = false;
+
 var setup_tty = function(){
-	var serialPort = new SerialPort("/dev/tty.usbmodem1412", {
+	var serialPort = new SerialPort("/dev/ttyACM0", {
 	  baudrate: 9600
 	});
 
 	serialPort.on('data', function(d){
-		console.log("data: "+d.toString());
+		if (d.toString().match(/0/)) {
+
+			var evt = is_recording? "stop" : "record";
+			is_recording = !!is_recording;
+
+			var rec = {
+				evt: evt,
+				data: {"track": "1", "clip": "1"}
+			};
+			client.sendText(JSON.stringify(rec));
+		}
 	});
 };
-
-found_server(server, port);
