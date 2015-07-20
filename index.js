4e8ae16e1f1b7b915d11150a1dbe92a023a76368
rob <rob@partidosurrealista.com>
2015-07-20 13:43:40 -0500

    implementando único botón and stuffs

--- index.js
+++ index.js
@@ -1,45 +1,158 @@
+
 var mdns = require('mdns');
 var ws = require('nodejs-websocket');
-var SerialPort = require("serialport").SerialPort;
+var sp = require("serialport");
+var SerialPort = sp.SerialPort;
+var glob = require('glob');
 
 var browser = mdns.createBrowser(mdns.tcp('livecontrol'));
-
 browser.on('serviceUp', function(service) {
-  console.log("service up: ", service.host);
-  var server = service.host;
-  var port = service.port;
-  found_server(server, port);
+  console.log("Server found at "+service.host);
+  connect(service.host, service.port);
+  browser.stop();
 });
-browser.start();
+
+var setup = function(){
+	console.log("Looking for server...");
+	browser.start();
+	client = null;
+	LIVE = false;
+};
+setup();
 
 var client = null;
+var pedales = {};
+var LIVE = false;
 
-var found_server = function(server, port){
-	client = ws.connect("ws://"+server+":"+port+"/control", function(){
-		console.log("WS connected");
+/*
+LEDS:
+-1	transition	blue
+0	off
+1	recording
+2	playing
+*/
+
+var scanUSB = function(){
+	console.log("Looking for devices");
+	glob("/dev/ttyACM*", function(err, files){
+		if (err) {
+			console.error(err);
+		} else {
+			files.forEach(function(file){
+				var pedal = new SerialPort(file, {parity: 'none', parser: sp.parsers.readline("\n")});
+				setTimeout(function(){
+					register(pedal);
+				}, 100);
+			});
+		}
+	});
+};
+scanUSB();
+
+var register = function(pedal) {
+	var id = null;
+	pedal.on("data", function(buffer){
+		if (buffer.match(/^id/)) {
+			id = buffer.replace(/\D/g, '');
+			console.log("Device registered with ID <"+id+">");
+			pedal.write("0");
+			pedales[id] = {serial: pedal, id: id, recording: false};
+		} else {
+			if (!LIVE) {
+				return false;
+			}
+
+			pedal = pedales[id];
+
+			if (buffer !== "0") {
+				pedal.expires = new Date().getTime() + 5000;
+				setTimeout(function(){
+					pedal.serial.write("3");
+				}, 5000);
+				// console.log("Buffer es:", buffer);
+			} else {
+				if (pedal.expires) {
+					var now = new Date().getTime();
+					var expires = pedal.expires;
+					pedal.expires = null;
+
+					if (now >= expires) {
+						console.log("DELETING!");
+						client.sendText(JSON.stringify({
+							evt: "clip:delete",
+							data: {track: id, clip: "0"}
+						}));
+						pedal.serial.write("0");
+						return true;
+					}
+				}
+
+				var evt = pedal.recording ? "stop" : "record";
+				pedal.recording = !pedal.recording;
+
+				var msg = {
+					evt: evt,
+					data: {"track": pedal.id, "clip": "0"}
+				};
+
+				console.log(msg);
+
+				client.sendText(JSON.stringify(msg));
+				pedal.serial.write("2");
+			}
+		}
 	});
 
-	setup_tty();
+	pedal.write("?", function(err, res){
+		if (err) {
+			console.error("Error querying ID", err);
+		} else {
+			console.log("Getting ID for "+pedal.path);
+		}
+	});
 };
 
-var is_recording = false;
-
-var setup_tty = function(){
-	var serialPort = new SerialPort("/dev/ttyACM0", {
-	  baudrate: 9600
+var connect = function(host, port){
+	console.log("Connecting to server...");
+	client = ws.connect("ws://"+host+":"+port+"/control", function ConnectedToServer(){
+		console.log("Connected to server at "+host+":"+port);
+		LIVE = true;
 	});
 
-	serialPort.on('data', function(d){
-		if (d.toString().match(/0/)) {
+	client.on('close', function(){
+		console.log("Lost server at "+host+":"+port);
+		setup();
+	});
 
-			var evt = is_recording? "stop" : "record";
-			is_recording = !!is_recording;
+	client.on('text', function(data){
+		var msg = JSON.parse(data);
 
-			var rec = {
-				evt: evt,
-				data: {"track": "1", "clip": "1"}
-			};
-			client.sendText(JSON.stringify(rec));
+		if (['tick', 'tock'].indexOf(msg.evt) == -1) {
+			console.log("Received: ", msg);
+		}
+
+		switch (msg.evt) {
+			// case 'tick':
+			// 	b[0] = 0x01;
+			// 	// serialPort.write("1");
+			// 	break;
+			// case 'tock':
+			// 	b[0] = 0x00;
+			// 	// serialPort.write(b);
+			// 	break;
+			case 'track:info':
+				console.log("track info: "+msg.data.status);
+				pedales[msg.data.track].serial.write(msg.data.status.toString());
+				break;
+				// if (data.data.status === "2") {
+				// 	console.log("Recording started for track "+data.data.track);
+				// 	serialPort.write("3");
+				// }
+				// break;
+			case 'clip:state':
+				console.log("clip state: "+msg.data.status);
+				pedales[msg.data.track].serial.write(msg.data.status.toString());
+				break;
 		}
 	});
 };
